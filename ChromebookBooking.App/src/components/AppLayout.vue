274769168ff4b81b6493304d24a364d8c121601a
João Bagatoli <bagatoli.dev@outlook.com>
2026-05-19 22:48:23 -0300

    feat: redirecionamento de acordo com role do usuário

---
+++ ChromebookBooking.App/src/components/AppLayout.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  import { useAuthStore } from '../stores/auth'
+  import AppSidebar from './AppSidebar.vue'
+
+  const authStore = useAuthStore()
+
+  const isAdmin = authStore.profile?.role === 'Admin'
+</script>
+
+<template>
+  <AppSidebar v-if="isAdmin" />
+  <RouterView></RouterView>
+</template>
+
+<style scoped>
+
+</style>
---
+++ ChromebookBooking.App/src/components/AppSidebar.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+  import { useAuthStore } from "../stores/auth"
+
+  const authStore = useAuthStore()
+  const modules = authStore.profile?.modules || []
+</script>
+
+<template>
+  <aside>
+    <nav>
+      <RouterLink
+        v-for="module in modules"
+        :key="module"
+        :to="{ name: module }">
+        {{ module }}
+      </RouterLink>
+    </nav>
+  </aside>
+</template>
+
+<style scoped>
+
+</style>
