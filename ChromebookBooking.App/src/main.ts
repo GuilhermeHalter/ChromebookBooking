55e95d9d170f0e53b678148ef893e1913eda5f13
João Bagatoli <bagatoli.dev@outlook.com>
2026-05-24 17:43:21 -0300

    feat: define preset styles (prime)

--- ChromebookBooking.App/src/main.ts
+++ ChromebookBooking.App/src/main.ts
@@ -3,7 +3,7 @@ import 'primeicons/primeicons.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import PrimeVue from 'primevue/config'
-import Aura from '@primeuix/themes/aura'
+import { preset } from './theme/preset'
 import AxiosHttpClient from './http/AxiosHttpClient'
 import ServiceFactory from './services/ServiceFactory'
 import { createPinia } from 'pinia'
@@ -13,7 +13,7 @@ import { useAuthStore } from './stores/auth'
 const pinia = createPinia()
 const app = createApp(App)
 
-app.use(PrimeVue, { theme: { preset: Aura } })
+app.use(PrimeVue, { theme: { preset } })
 app.use(pinia)
 
 const authStore = useAuthStore()
